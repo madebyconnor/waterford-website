a837eaf2d4999b3637fcee3fe08d110bd00bfe02
Connor Bär <connor.baer@me.com>
2016-03-28 20:43:56 +0200

    Factors sidebar into separate region

--- perch/templates/pages/article.php
+++ perch/templates/pages/article.php
@@ -3,10 +3,15 @@
   'template' => 'Article/Article_content.html',
   )); 
 ?>
+<?php perch_content_create('Sidebar', array(
+  'template' => 'Article/Article_sidebar.html',
+  )); 
+?>
 <?php perch_layout('header'); ?>
 <?php perch_layout('meta'); ?>
 <?php perch_layout('navigation'); ?>
-<span itemscope itemtype="https://schema.org/Article">
+<span <?php perch_content_custom('Sidebar', array(
+    'filter'=>'sidebar','match'=>'eq','value'=>'hasSidebar','template'=>'#/sidebarid.html')); ?> itemscope itemtype="https://schema.org/Article">
 <header id="margin-subnav" class="b-grey-d <?php
       perch_page_attribute('headerclass', array(
         'template' => 'margin_subnav.html'
@@ -24,7 +29,22 @@
 
 <section>
   <div class="article-background">
-    <?php perch_content('Content'); ?>
+    <div class="m0a article-wrapper">
+      <div id="article">
+        <span itemprop"articleBody">
+          <?php perch_content('Content'); ?>
+        </span>
+        <!-- Social -->
+        <div class="article-footer text-center">
+          <ul id="social">
+            <li class="social-item"><a class="social-icon facebook no-link" href="javascript:window.open('https://www.facebook.com/sharer/sharer.php?u=<?php perch_page_url(); ?>)', 'Facebook', 'width=600, height=400' )"><i class="icon-facebook"></i></a></li>
+            <li class="social-item"><a class="social-icon twitter no-link" href="javascript:window.open('http://twitter.com/share?text=Take%20a%20look%20at%20this%20page%20on%20Waterford.sz&url=<?php perch_page_url(); ?>', 'Twitter', 'width=600, height=400' )"><i class="icon-twitter"></i></a></li>
+            <li class="social-item"><a class="social-icon email no-link" href="mailto:?subject=Take%20a%20look%20at%20this%20page%20on%20Waterford.sz&amp;body=<?php perch_page_url(); ?>"><i class="icon-email"></i></a></li>
+          </ul>
+        </div>
+      </div>
+      <?php perch_content('Sidebar'); ?>
+    </div>
   </div>
 </section>
 </span>
